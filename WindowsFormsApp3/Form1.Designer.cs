d89304af299eaf8849b29c95f056580e1043273c
Nokijoto <51673810+Nokijoto@users.noreply.github.com>
2020-10-26 12:03:18 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pp = new System.Windows.Forms.Button();
+            this.subs = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pp
+            // 
+            this.pp.Location = new System.Drawing.Point(86, 45);
+            this.pp.Name = "pp";
+            this.pp.Size = new System.Drawing.Size(75, 23);
+            this.pp.TabIndex = 0;
+            this.pp.Text = "Otwórz";
+            this.pp.UseVisualStyleBackColor = true;
+            this.pp.Click += new System.EventHandler(this.pp_Click);
+            // 
+            // subs
+            // 
+            this.subs.Location = new System.Drawing.Point(34, 90);
+            this.subs.Multiline = true;
+            this.subs.Name = "subs";
+            this.subs.ReadOnly = true;
+            this.subs.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.subs.Size = new System.Drawing.Size(688, 327);
+            this.subs.TabIndex = 1;
+            this.subs.Text = "TEST";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Załaduj";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.subs);
+            this.Controls.Add(this.pp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pp;
+        private System.Windows.Forms.TextBox subs;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string[] fileLine;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pp_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Aegisubs (*.ass)|*.ass "+/*|txt files (*.txt)|*.txt|*/"|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    var lineCount = File.ReadLines(filePath).Count();
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        subs.Text += fileContent;
+                        //System.Console.Out.Write.
+                       /* for (int i = 1; i < lineCount; i++)
+                            fileLine[i] = fileContent;
+                            subs.Text = fileLine[0];*/
+                        // fileLine.ReadLine();
+                        //return fileLine[];
+
+                    }
+                    //subs.Text = fileLine;
+                   // subs.Text = fileContent;
+                }
+            }
+        }
+
+        private void pp_Click_2(object sender, EventArgs e)
+        {
+           // pp_Click_2(fileLine[]);
+        }
+    }
+}
